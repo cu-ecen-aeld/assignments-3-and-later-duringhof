743ea3925e247429028c1067d58b6418d514dca9
Hans-Martin Duringhof <hmduringhof@gmail.com>
2023-02-18 21:35:40 +0100

    Complete assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -eq 2 ]; then
+	filesdir=$1
+	searchstr=$2
+else
+	echo "Error: Invalid Number of Arguments"
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "	1) File Directory Path."
+	echo "	2) String to be searched in the specified directory path."
+	exit 1
+fi
+if [ -d "$filesdir" ]; then
+	file_count=$(ls $filesdir | wc -l)
+	match_count=$(grep -r $searchstr $filesdir | wc -l)
+	echo "The number of files are $file_count and the number of matching lines are $match_count"
+	exit 0
+else
+	echo "Error: Argument 1 is not a directory"
+        exit 1
+fi
---
+++ finder-app/writer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -eq 2 ]; then
+	writefile=$1
+	writestr=$2
+else
+	echo "Error: Invalid Number of Arguments"
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "	1) Full path to a file including file name."
+	echo "	2) String to be written in the specified file."
+	exit 1
+fi
+touch2() { mkdir -p "$(dirname "$1")" && touch "$1" ; }
+touch2 $writefile
+if [ $? -eq 0 ]; then
+	echo $writestr > $writefile
+	exit 0
+else
+	echo "Error: File could not be created"
+        exit 1
+fi
