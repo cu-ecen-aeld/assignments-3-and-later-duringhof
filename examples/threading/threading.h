859d193374df3f2c2a7f64f16340ac9038ea9318
Hans-Martin <hmduringhof@gmail.com>
2023-03-03 22:46:57 +0100

    Solution assignment 4 part 1

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -8,18 +8,12 @@
  * the joiner thread.
  */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
 
-    /**
-     * Set to true if the thread completed with success, false
-     * if an error occurred.
-     */
     bool thread_complete_success;
+    int wait_ms_obtain;
+    int wait_ms_release;
+    pthread_mutex_t *mutex_lock;
+    pthread_t thread_id;
 };
 
 
